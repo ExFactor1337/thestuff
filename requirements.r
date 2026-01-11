155febd37b78ee26a73221e3068036ba5e0c7d2a
ExFactor <excel.que@gmail.com>
2026-01-10 19:13:36 -0500

    added generic r image to CI

---
+++ requirements.r
@@ -0,0 +1,6 @@
+# Hashtag comments are OK in this file
+# R requirement files don't need versions, as they are tied to a static MRAN snapshot 
+# MRAN snapshot is tied to the rocker version, so there is your version control
+dplyr
+ggplot2
+data.table
